63700158050d2cebdb12d59699fed4d97b27cac5
berge <berge@prohotel-edv.de>
2019-11-18 19:59:56 +0100

    Wörter zählen in Clown Text

--- lernen.py
+++ lernen.py
@@ -1,1 +1,19 @@
-print ("hello world")
+fname = input("Datei: ")
+if len(fname) < 1 : fname = "clown.txt"
+hand = open(fname)
+
+di = dict()
+for lin in hand:
+    lin=lin.rstrip()
+    #print(lin)
+    wds = lin.split()
+    #print(wds)
+    for w in wds:
+        print(w)
+        if w in di :
+            di[w] = di[w] + 1
+            print("***gibts schon***")
+        else:
+            di[w] = 1
+            print("***NEW***")
+        print(di[w])
